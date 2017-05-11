47f72b65762cab49bc34a2b298e64c13e4fcf79b
JackyRecelis <jackymlui@live.com>
2017-05-11 17:17:44 +1000

    changing style

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+ $(document).ready(function(){
+      $("#About").on("click",function(event) {
+        var offset = -60; //Offset of 20px
+
+        $('html, body').animate({
+            scrollTop: $("#Aboutsection").offset().top + offset
+        }, 2000);
+    });
+
+    $("#Portfolio").on("click",function(event) {
+        var offset = -60; //Offset of 20px
+
+        $('html, body').animate({
+            scrollTop: $("#Portfoliosection").offset().top + offset
+        }, 2000);
+    });
+
+    $("#Contact").on("click",function(event) {
+        var offset = 0; //Offset of 20px
+
+        $('html, body').animate({
+            scrollTop: $("#ContactMesection").offset().top + offset
+        }, 2000);
+    });  
+
+    $("#Top").on("click",function(event) {
+        var offset = 0; //Offset of 20px
+
+        $('html, body').animate({
+            scrollTop: $("#Home").offset().top + offset
+        }, 2000);
+    });
+
+  });
