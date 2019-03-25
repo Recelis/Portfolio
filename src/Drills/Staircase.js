eabd7bf02237dadc23174372fe56a6ba6c905093
Jacky Lui <jackymlui@live.com>
2019-03-25 17:40:35 +1000

    solved mini-max cleverly

--- src/Drills/Staircase.js
+++ src/Drills/Staircase.js
@@ -1,8 +1,8 @@
 function staircase(n){
-    for (let ii =1; ii <= n; ii++){
+    for (let ii =1; ii <= n; ii++){ // for loop within for loop
         let stair = "";
         for (let jj =1; jj <= n; jj++){
-            if (jj > n-ii) stair += "#";
+            if (jj > n-ii) stair += "#"; // backwards counting
             else stair += " ";
         }
         console.log(stair);
