ddb47c28814540152795bdc38e73fcb69a0a99eb
Jacky Lui <jackymlui@live.com>
2019-03-29 13:57:48 +1000

    refactored repeated string

--- src/Drills/Repeated String.js
+++ src/Drills/Repeated String.js
@@ -3,25 +3,21 @@ n = 10;
 function repeatedString(s,n){
     // calculate number of times needed to repeated string
     let repeatTimes = Math.floor(n/s.length);
-    console.log(repeatTimes);
     // find number of a's in string
-    let ass = s.match(/[a]/g);
-    if (ass === null) ass = 0;
-    else{
-        ass = ass.length;
-    } 
-    let flooredAs = ass*repeatTimes;
-    console.log(flooredAs);
-    console.log(n%s.length);
+    let matched = getAs(s);
+    let flooredAs = matched*repeatTimes;
     // get a's in modString
     let modString = s.slice(0,n%s.length)
-    console.log(modString);
     // get a's in modString
-    let newAss = modString.match(/[a]/g);
-    if (newAss === null) newAss = 0;
-    else newAss = newAss.length;
-    return newAss + flooredAs;
-    
+    let modAs = getAs(modString);
+    return modAs + flooredAs;
+}
+
+function getAs(myString){
+    let matched = myString.match(/[a]/g);
+    if (matched === null) matched = 0;
+    else matched = matched.length;
+    return matched;
 }
 
 console.log(repeatedString(s,n));
