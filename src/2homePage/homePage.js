07e270a13811de55ef816463e04fcac1321121c8
Jacky Lui <jackymlui@live.com>
2019-04-14 21:19:38 +1000

    left align working

--- src/2homePage/homePage.js
+++ src/2homePage/homePage.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
 
-import { Row, Col, Media } from "reactstrap";
+import Grid from "@material-ui/core/Grid";
 import image from "../images/jacky.png";
 
 class HomePage extends Component {
   render() {
     return (
       <section className="home">
-        <Row>
-          <Col xs={12} md={6}>
-            <Media
+        <Grid container>
+          <Grid xs={12} md={6}>
+            <img
               className="home__image-large"
               src={image}
               alt="person sternly looking into the souls of the viewers"
               rounded
               responsive
             />
-          </Col>
-          <Col xs={12} md={6}>
+          </Grid>
+          <Grid xs={12} md={6}>
             <p className="home__heading">Run. Code. Connect.</p>
             <p className="home__tagline">
               Hi, I'm Jacky Lui. A web developer with a passion for well written clean + tidy
               code.
             </p>
-          </Col>
-          <Col xs={12} md={6}>
-          <Media
+          </Grid>
+          <Grid xs={12} md={6}>
+          <img
               className="home__image-small"
               src={image}
               alt="person sternly looking into the souls of the viewers"
               rounded
               responsive
             />
-            </Col>
-        </Row>
+            </Grid>
+        </Grid>
       </section>
     );
   }
