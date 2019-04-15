b1f205b943b851ee170acd379b7e66e919d797d1
Jacky Lui <jackymlui@live.com>
2019-04-15 11:16:29 +1000

    changed favicon

--- src/6contact/contact.js
+++ src/6contact/contact.js
@@ -2,8 +2,33 @@ import React, { Component } from "react";
 
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import { throws } from "assert";
+import { ENETDOWN } from "constants";
 
 export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangleMessage = this.handleChangleMessage.bind(this);
+  }
+  handleChangeName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleChangeEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleChangleMessage(event) {
+    this.setState({ message: event.target.value });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
   }
@@ -18,12 +43,19 @@ export default class Contact extends Component {
           <form>
             <div className="contact__form-group">
               <label for="name">Name</label>
-              <input type="text" className="form-control" id="name" />
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChangeName}
+                id="name"
+              />
             </div>
             <div className="contact__form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input
                 type="email"
+                value={this.state.email}
+                onChange={this.handleChangeEmail}
                 className="form-control"
                 id="email"
                 aria-describedby="emailHelp"
@@ -31,7 +63,10 @@ export default class Contact extends Component {
             </div>
             <div className="contact__form-group">
               <label for="message">Message</label>
-              <textarea className="form-control" rows="5" id="message" />
+              <textarea 
+              value={this.state.message}
+              onChange={this.handleChangleMessage}
+              className="form-control" rows="5" id="message" />
             </div>
             <button type="submit" className="btn btn-primary">
               Submit
